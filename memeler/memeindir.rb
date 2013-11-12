748bf33976109e2a38abf4b7f0e04251fa8313ee
Arkadaki Dede <dedecan@mailinator.com>
2013-11-12 03:59:42 +0000

    adds new memes and meme downloader

---
+++ memeler/memeindir.rb
@@ -0,0 +1,38 @@
+
+
+memes = [
+"http://www.ledrome.com/forum/uploads/images/2yoyoyo.gif",
+"http://i.imgur.com/Rd5Hc.gif",
+"http://i.imgur.com/j6nsj.gif",
+"http://img.pandawhale.com/66297-katee-owen-dancing-boobs-gif-k-Qmsk.gif",
+"http://coedmagazine.files.wordpress.com/2012/11/bouncing-boobs.gif",
+"http://i.minus.com/ibsDxvQdb9VAJO.gif",
+"http://forgifs.com/gallery/d/60863-4/busting_out.gif",
+"https://lh3.googleusercontent.com/-mos3AgcxTGg/UYUlJYqINWI/AAAAAAAABy0/3azFwDr1ad4/w288-h288/grande-fratello-amazing-hot-girl-touching-her-boobs-animated-gif.gif",
+"http://files.sharenator.com/glorious_boobs_gifs_running_Maintain_Eye_Contact_5-s234x250-391116-580.gif",
+"http://media2.giphy.com/media/ARBJPjYFWlXIk/giphy.gif",
+"http://coltmonday.files.wordpress.com/2010/10/jennifer-love-hewitt-bouncy-boobs-mopping-up.gif",
+"http://coedmagazine.files.wordpress.com/2013/07/bouncing-boobs-gifs-49.gif",
+"http://theblackduck.net/pix/bouncing-boobs-o.gif",
+"http://files.sharenator.com/glorious_boobs_gifs_ring_Maintain_Eye_Contact_5-s410x307-391115-580.gif",
+"http://coedmagazine.files.wordpress.com/2013/04/lindsay-lohan-boobs-gif-19.gif",
+"http://img.izismile.com/img/img6/20130724/1000/gifs_give_bouncing_boobs_some_extra_oomph_22.gif",
+"http://i.imgur.com/YkKCGeX.gif",
+"http://thewootapeletters.files.wordpress.com/2012/04/fba4a3603e592.gif",
+"http://cdn.acidcow.com/pics/20130426/sofia_vergara_03.gif",
+"http://m.brobible.com/files/uploads/images/bro-life/ask-a-bro/boobs-gif.gif",
+"http://therynoshorn.com/wp-content/uploads/2012/08/big-bouncing-boobs-gif-12.gif",
+"http://i.imgur.com/yAcGQcH.gif",
+"http://desmond.imageshack.us/Himg202/scaled.php?server=202&filename=7e992origbouncinggif.gif&res=medium",
+"http://i.imgur.com/ghrB5bm.gif",
+"http://media2.giphy.com/media/DuTEYXP3CEGCA/giphy.gif",
+"http://img.pandawhale.com/66296-Katee-Owen-bicycle-boobs-gif-bP90.gif",
+"http://theblackduck.net/pix/Bounce_green_shirted_boobs.gif",
+"http://cafom.s3.amazonaws.com/wp-content/uploads/bouncing-boobs.gif"
+]
+
+i = 56
+memes.each do |meme|
+  `wget -O #{i}.gif #{meme}`
+  i = i + 1
+end
